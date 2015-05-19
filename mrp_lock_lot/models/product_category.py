caed84dafa2e03ade46ca1634b1fb56decb93167
Lionel Sausin <ls@numerigraphe.com>
2015-05-19 15:11:11 +0200

    [WIP] indicate if parent categories lock lots
    This implementation does not work completly because it does not propagate the value recursivly to all chlidren's children

--- mrp_lock_lot/models/product_category.py
+++ mrp_lock_lot/models/product_category.py
@@ -2,7 +2,7 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class ProductCategory(models.Model):
@@ -12,3 +12,19 @@ class ProductCategory(models.Model):
         string='Block new Serial Numbers/lots',
         help='If checked, future Serial Numbers/lots will be created blocked '
              'by default')
+
+    parent_lot_default_locked = fields.Boolean(
+       string='Block new Serial Nb. (incl. parents)',
+       compute='_get_parent_lot_default_locked', store=True,
+       help='Checked automatically when "Block new Serial Numbers/lots" is '
+            'checked on either this Category or one of its parents.')
+
+    @api.one
+    @api.depends('lot_default_locked', 'parent_id',
+                 'parent_id.lot_default_locked', 'parent_id.parent_lot_default_locked')
+    def _get_parent_lot_default_locked(self):
+        """Locked (including categories)
+
+        @return True when the category or one of the parents demand new lots
+                to be locked"""
+        self.parent_lot_default_locked = self.lot_default_locked or self.parent_id.lot_default_locked
